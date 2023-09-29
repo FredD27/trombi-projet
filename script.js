c2e7192d9d9bf10af6e0280a4878c085d9805c28
Florian BRUN <flo33.brun@hotmail.fr>
2023-09-29 10:31:51 +0200

    almost finished project

---
+++ script.js
@@ -0,0 +1,174 @@
+// const persons = [
+//     {
+//         photo: "Photos/Les 4 Fantastiques/IMG_0037.jpg",
+//         firstName: "Sylvain",
+//         lastName: "BONNAURE"
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "David",
+//         lastName: "DURANTY"
+//     },
+    
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Antoine",
+//         lastName: "CAIREY"
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Victor",
+//         lastName: "GUYOMARD"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Mathieu",
+//         lastName: "CHAUVEAU"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Rapahël",
+//         lastName: "BARD"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Cédric",
+//         lastName: "DUPOND"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Mahdi",
+//         lastName: "MCHEIK"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Marie",
+//         lastName: "DELAIRE"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Nassime",
+//         lastName: "HARMACH"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Cassiopée",
+//         lastName: "LAURIE"
+
+//     },
+    
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Anaëlle",
+//         lastName: "CAUVIN"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Aurore",
+//         lastName: "VALLEIX"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Benoit",
+//         lastName: "HAYET"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Leslie",
+//         lastName: "ABAJOLI"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Lucas",
+//         lastName: "BORDES"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Claire",
+//         lastName: "VANNIER"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Nelia",
+//         lastName: "COURTAIS"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Florian",
+//         lastName: "BRUN"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Victor",
+//         lastName: "GARCIA"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Adam",
+//         lastName: "HEMAMOU"
+
+//     },
+
+//     {
+//         photo: "Photos/The OG/IMG_9997.jpg",
+//         firstName: "Frédérique",
+//         lastName: "DRUET"
+
+//     },
+
+
+
+
+   
+// ];
+
+// const container = document.querySelector(".container");
+// console.log(container);
+
+// persons.forEach((person) => {
+//     const card = createPersonCard(person);
+//     container.appendChild(card);
+// });
+
+// function createPersonCard(person) {
+//     const card = document.createElement("div")
+//     card.classList.add("element");
+//     card.innerHTML= `
+//     <img src="${person.photo} alt="${person.firstName}"> 
+//     <h3>${person.firstName} ${person.lastName}</h3>`
+//     return(card)
+// };
